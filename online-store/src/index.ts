a3d90d896292aa8a395522d0ffd2f7604a84f5f6
Yulia Bondar <yuliabondar98@gmail.com>
2022-12-23 00:01:12 +0300

    refactor: add line

--- online-store/src/index.ts
+++ online-store/src/index.ts
@@ -1,24 +1,24 @@
-import Home from "./components/home";
-import { LegoItem } from "./components/types";
+import Home from './components/home';
+import { LegoItem } from './components/types';
 
-const dataSource =
-  "https://raw.githubusercontent.com/poznerrr/fakedb/main/db.json";
+const dataSource = 'https://raw.githubusercontent.com/poznerrr/fakedb/main/db.json';
+
 fetch(dataSource)
-  .then((response) => response.json())
-  .then((data) => {
-    const app = new App(data);
-    app.start();
-  });
+    .then((response) => response.json())
+    .then((data) => {
+        const app = new App(data);
+        app.start();
+    });
 
 class App {
-  data: Array<LegoItem>;
+    data: Array<LegoItem>;
 
-  constructor(data: Array<LegoItem>) {
-    this.data = data;
-  }
+    constructor(data: Array<LegoItem>) {
+        this.data = data;
+    }
 
-  async start() {
-    const home = new Home(this.data);
-    await home.render();
-  }
+    async start() {
+        const home = new Home(this.data);
+        await home.render();
+    }
 }
