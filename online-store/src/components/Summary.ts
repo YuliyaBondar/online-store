15c4454dd7ab4185f1c85c3eb71432636c4ef362
poznerrr <pozner@gmail.com>
2023-01-04 18:13:37 +0300

    feat: add all logic from order module window

--- online-store/src/components/Summary.ts
+++ online-store/src/components/Summary.ts
@@ -1,4 +1,4 @@
-import { promos } from './constants';
+import { promos, cardsUrl } from './constants';
 import { IComponent } from './interfaces';
 import DiscountList from './DiscountList';
 
@@ -8,6 +8,14 @@ import '../styles/summary.css';
 
 class Summary implements IComponent {
   discountList: DiscountList;
+  isAllValid = true;
+  isValidName: boolean = false;
+  isValidNumber: boolean = false;
+  isValidDelivery: boolean = false;
+  isValidEmail: boolean = false;
+  isValidCardNumber: boolean = false;
+  isValidValid: boolean = false;
+  isValidCVV: boolean = false;
   constructor() {
     this.discountList = new DiscountList();
   }
@@ -39,22 +47,44 @@ class Summary implements IComponent {
       Personal Detail
     </div>
     <div class="buy-popap__name">
-      <input>
+      <input class="buy-popap__name-input buy-popap__input" placeholder="Name">
+      <p class="popap-error popap-error-name popap-error_hidden">error</p>
     </div>
     <div class="buy-popap__phone">
-      <input>
+      <input class="buy-popap__phone-input buy-popap__input" placeholder="Phone number">
+      <p class="popap-error popap-error-phone popap-error_hidden">error</p>
     </div>
     <div class="buy-popap__delivery">
-      <input>
+      <input class="buy-popap__delivery-input buy-popap__input" placeholder="Delivery address">
+      <p class="popap-error popap-error-delivery popap-error_hidden">error</p>
     </div>
     <div class="buy-popap__email">
-      <input>
+      <input class="buy-popap__email-input buy-popap__input" placeholder="E-mail">
+      <p class="popap-error popap-error-email popap-error_hidden">error</p>
     </div>
     <div class="buy-popap__header">
     Credit card details
     </div>
-    <div class="buy-popap__card"></div>
-    <div class="buy-popap__errors"></div>
+    <div class="buy-popap__card credit-card">
+    <div class="credit-card__identity">
+      <div class="credit-card__img-div"><img class="credit-card__img"></div>
+      <input class="credit-card__number-input" placeholder="Card number">
+    </div>
+    <div class="credit-card__additional">
+      <div class="credit-card__valid">
+      <span>VALID:</span>
+      <input class="credit-card__valid-input" placeholder="Valid Thru">
+      </div>
+      <div class="credit-card__cvv">
+      <span>CVV:</span>
+      <input class="credit-card__cvv-input" placeholder="Code" type="number"></div>
+    </div>
+    </div>
+    <div class="buy-popap__card-errors">
+    <p class="popap-error-card popap-error popap-error_hidden">Card number - Error</p>
+    <p class="popap-error-valid popap-error popap-error_hidden">Card valid thru - Error</p>
+    <p class="popap-error-cvv popap-error popap-error_hidden" >Card CVV - Error</p>
+    </div>
     <button class="buy-popap__confirm">Confirm</button>
     </div>
     </div>
@@ -84,6 +114,121 @@ class Summary implements IComponent {
     const popapConfirm = document.querySelector('.buy-popap__confirm');
     popapConfirm?.addEventListener('click', () => {
       this.checkConfirm();
+    });
+
+    const inputCardNumber = document.querySelector('.credit-card__number-input');
+    inputCardNumber?.addEventListener('input', (e: Event) => {
+      this.changePaySystem(e);
+    });
+    inputCardNumber?.addEventListener('input', () => {
+      const cardNumberError = document.querySelector('.popap-error-card');
+      if ((<HTMLInputElement>inputCardNumber).value.length !== 16) {
+        (<HTMLElement>cardNumberError).classList.remove('popap-error_hidden');
+        this.isValidCardNumber = false;
+      } else {
+        (<HTMLElement>cardNumberError).classList.add('popap-error_hidden');
+        this.isValidCardNumber = true;
+      }
+    });
+
+    const inputName = document.querySelector('.buy-popap__name-input');
+    inputName?.addEventListener('input', () => {
+      const cardNameError = document.querySelector('.popap-error-name');
+      const re = new RegExp('^[А-ЯA-Za-zа-я0-9-]*$');
+      let names = (<HTMLInputElement>inputName).value.split(' ');
+      names = names.map((item) => item.trim()).filter((item) => !!item);
+      if (names.length < 2 || names.filter((item) => re.test(item) && item.length >= 3).length !== names.length) {
+        (<HTMLElement>cardNameError).classList.remove('popap-error_hidden');
+        this.isValidName = false;
+      } else {
+        (<HTMLElement>cardNameError).classList.add('popap-error_hidden');
+        this.isValidName = true;
+      }
+    });
+
+    const inputNumber = document.querySelector('.buy-popap__phone-input');
+    inputNumber?.addEventListener('input', () => {
+      const cardPhoneError = document.querySelector('.popap-error-phone');
+      const re = new RegExp('^[+][0-9]{9,}$');
+      if (!re.test((<HTMLInputElement>inputNumber).value)) {
+        (<HTMLElement>cardPhoneError).classList.remove('popap-error_hidden');
+        this.isValidNumber = false;
+      } else {
+        (<HTMLElement>cardPhoneError).classList.add('popap-error_hidden');
+        this.isValidNumber = true;
+      }
+    });
+
+    const inputDelivery = document.querySelector('.buy-popap__delivery-input');
+    inputDelivery?.addEventListener('input', () => {
+      const cardDeliveryError = document.querySelector('.popap-error-delivery');
+      const re = new RegExp('^[A-ZА-Яa-zа-я0-9-]*$');
+      let names = (<HTMLInputElement>inputDelivery).value.split(' ');
+      names = names.map((item) => item.trim()).filter((item) => !!item);
+      if (names.length < 3 || names.filter((item) => re.test(item) && item.length >= 5).length !== names.length) {
+        (<HTMLElement>cardDeliveryError).classList.remove('popap-error_hidden');
+        this.isValidDelivery = false;
+      } else {
+        (<HTMLElement>cardDeliveryError).classList.add('popap-error_hidden');
+        this.isValidDelivery = true;
+      }
+    });
+
+    const inputEmail = document.querySelector('.buy-popap__email-input');
+    inputEmail?.addEventListener('input', () => {
+      const cardEmailError = document.querySelector('.popap-error-email');
+      const re = new RegExp('^[a-zA-Z0-9-.,]{2,}@[a-zA-Z]{2,}[.][a-zA-Z]{2,}$');
+      if (!re.test((<HTMLInputElement>inputEmail).value)) {
+        (<HTMLElement>cardEmailError).classList.remove('popap-error_hidden');
+        this.isValidEmail = false;
+      } else {
+        (<HTMLElement>cardEmailError).classList.add('popap-error_hidden');
+        this.isValidEmail = true;
+      }
+    });
+
+    const inputCVV = document.querySelector('.credit-card__cvv-input');
+    inputCVV?.addEventListener('input', () => {
+      const cardCVVError = document.querySelector('.popap-error-cvv');
+      const re = new RegExp('^[0-9]{3}$');
+      if (!re.test((<HTMLInputElement>inputCVV).value)) {
+        (<HTMLElement>cardCVVError).classList.remove('popap-error_hidden');
+        this.isValidCVV = false;
+      } else {
+        (<HTMLElement>cardCVVError).classList.add('popap-error_hidden');
+        this.isValidCVV = true;
+      }
+    });
+
+    const inputValid = document.querySelector('.credit-card__valid-input');
+    inputValid?.addEventListener('input', (e: Event) => {
+      const re = new RegExp('^[0-9]{2}$');
+      const cardValidError = document.querySelector('.popap-error-valid');
+      if ((<HTMLInputElement>inputValid).value.length == 2) {
+        if (re.test((<HTMLInputElement>inputValid).value)) {
+          let separator = (<HTMLInputElement>inputValid).value;
+          separator += '/';
+          (<HTMLInputElement>inputValid).value = separator;
+          (<HTMLInputElement>inputValid).textContent = separator;
+        }
+      } else if ((<HTMLInputElement>inputValid).value.length > 5) {
+        (<HTMLInputElement>inputValid).value = (<HTMLInputElement>inputValid).value.slice(0, 5);
+      }
+
+      let numbers = (<HTMLInputElement>inputValid).value.split('/');
+      numbers = numbers.map((item) => item.trim()).filter((item) => !!item);
+      if (
+        numbers.length !== 2 ||
+        numbers.filter((item) => re.test(item)).length !== numbers.length ||
+        Number(numbers[0]) > 12 ||
+        Number(numbers[0]) < 1
+      ) {
+        (<HTMLElement>cardValidError).classList.remove('popap-error_hidden');
+        this.isValidValid = false;
+      } else {
+        (<HTMLElement>cardValidError).classList.add('popap-error_hidden');
+        this.isValidValid = true;
+      }
     });
   }
 
@@ -140,15 +285,49 @@ class Summary implements IComponent {
   }
 
   checkConfirm() {
-    //TODO: сделать валидацию всех полей
+    this.isAllValid = true;
+    this.booleanChecker(this.isValidName, '.popap-error-name');
+    this.booleanChecker(this.isValidNumber, '.popap-error-phone');
+    this.booleanChecker(this.isValidDelivery, '.popap-error-delivery');
+    this.booleanChecker(this.isValidEmail, '.popap-error-email');
+    this.booleanChecker(this.isValidCardNumber, '.popap-error-card');
+    this.booleanChecker(this.isValidValid, '.popap-error-valid');
+    this.booleanChecker(this.isValidCVV, '.popap-error-cvv');
+
     const popap = document.querySelector('.basket-design__buy-popap');
-    if (true) {
+    if (this.isAllValid) {
       (<HTMLElement>popap).innerHTML = `Congrats. Your buy is succesfull`;
       setTimeout(() => {
         AppState.instance.state.promos = [];
         AppState.instance.state.basket.orders = [];
         AppState.instance.state.app?.start();
       }, 5000);
+    }
+  }
+
+  booleanChecker(name: boolean, p: string): void {
+    if (!name) {
+      this.isAllValid = false;
+      const parag = document.querySelector(p);
+      (<HTMLElement>parag).classList.remove('popap-error_hidden');
+    }
+  }
+
+  changePaySystem(e: Event) {
+    const cardImg = document.querySelector('.credit-card__img');
+    switch ((<HTMLInputElement>e.target).value[0]) {
+      case '3':
+        (<HTMLImageElement>cardImg).src = cardsUrl.americanExpress;
+        break;
+      case '4':
+        (<HTMLImageElement>cardImg).src = cardsUrl.visa;
+        break;
+      case '5':
+        (<HTMLImageElement>cardImg).src = cardsUrl.masterCard;
+        break;
+      default:
+        (<HTMLImageElement>cardImg).src = ``;
+        break;
     }
   }
 }
--- online-store/src/components/constants.ts
+++ online-store/src/components/constants.ts
@@ -4,3 +4,11 @@ export const promos: Promo[] = [
   { name: 'LEGO', discount: 10, description: 'our site give discount!' },
   { name: '2023', discount: 20, description: 'happy new year 2023!' },
 ];
+
+export const cardsUrl = {
+  americanExpress:
+    'https://golfreviewsguide.com/wp-content/uploads/2021/10/The-American-Express-Logo.png?ezimgfmt=rs:311x187/rscb1/ngcb1/notWebP',
+  visa: 'https://www.tadviser.ru/images/8/86/Visa_Inc._logo.svg.png',
+  masterCard:
+    'https://play-lh.googleusercontent.com/czro-ULAemRM1bMldf9gHQ7ajfa9NzKiZXFjI85mxawo60CaKMyHsjWaM38KHiZpsgY',
+};
