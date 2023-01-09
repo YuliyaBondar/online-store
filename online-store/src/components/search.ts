83f3b34dd4ab7e2d9d29f3d904f15df31e419af7
Yulia Bondar <yuliabondar98@gmail.com>
2023-01-09 19:46:42 +0300

    feat: change search component

--- online-store/src/components/search.ts
+++ online-store/src/components/search.ts
@@ -14,6 +14,11 @@ export class Search implements IComponent {
     searchElement.value = AppState.instance.state.searchInput;
     searchElement.focus();
     searchElement.addEventListener('input', () => this.searchToy());
+    const clearSearch = <HTMLElement>document.querySelector('.cross');
+    clearSearch.addEventListener('click', () => {
+      (<HTMLInputElement>searchElement).value = '';
+      this.searchToy();
+    })
   }
 
   render() {
@@ -29,9 +34,27 @@ export class Search implements IComponent {
     let filteredToysList: Array<LegoItem> = [...AppState.instance.state.products];
     const searchElement = <HTMLInputElement>document.querySelector('.search');
     filteredToysList = filteredToysList.filter((toyItem) => {
-      const substring: string = (<HTMLInputElement>searchElement).value.toUpperCase();
-      const toyName: string = toyItem.name.toUpperCase();
-      return toyName.includes(substring);
+      const substring: string = (<HTMLInputElement>searchElement).value.toLowerCase();
+      const toyName: string = toyItem.name.toLowerCase();
+      const toyAgeFrom: string = toyItem.ageFrom.toString();
+      const toyPrice: string = toyItem.price.toString();
+      const toyCategory: string = toyItem.category.toLowerCase();
+      const toyNumbOfDetails: string = toyItem.numbOfDetails.toString();
+      const toySizeOfDetails: string = toyItem.sizeOfDetails.toLowerCase();
+      const toyInterests: string = toyItem.interests.toString().toLowerCase();
+      const toyDescription: string = toyItem.description.toLowerCase();
+      const toyAmountOnStock: string = toyItem.amountOnStock.toString();
+      return (
+        toyName.includes(substring) ||
+        toyAgeFrom.includes(substring) ||
+        toyPrice.includes(substring) ||
+        toyCategory.includes(substring) ||
+        toyNumbOfDetails.includes(substring) ||
+        toySizeOfDetails.includes(substring) ||
+        toyInterests.includes(substring) ||
+        toyDescription.includes(substring) ||
+        toyAmountOnStock.includes(substring)
+      );
     });
     AppState.instance.state.searchInput = searchElement.value;
     console.log(filteredToysList);
